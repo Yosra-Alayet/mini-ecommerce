a3f144ccaeb12a6004fd06e2bd2d18c8cb3864d0
Yosra-Alayet <alayetyosra@gmail.com>
2024-09-05 21:06:28 +0100

    Revert "US_1_Template_Integration"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { StoreComponent } from './pages/store/store.component';
 
-const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  {path:'store', component: StoreComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirection vers /home
-  { path: '**', redirectTo: '/home' } // Redirection en cas de chemin invalide
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { StoreComponent } from './pages/store/store.component';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { NewsletterComponent } from './layouts/newsletter/newsletter.component';
-import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    StoreComponent,
-    NavbarComponent,
-    HomeComponent,
-    NewsletterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
